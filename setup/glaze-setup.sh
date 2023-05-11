4d47211616f2d3aba4abef6c6be278b24aeaa20a
DoctorAti <atishayatishayjainjain@gmail.com>
2023-05-11 20:53:38 +0530

    changes

--- setup/glaze-setup.sh
+++ setup/glaze-setup.sh
@@ -2,7 +2,7 @@
 # script.sh
 
 # Fetch docker-compose.yml
-curl -O https://url-to-your-server/docker-compose.yml
+curl -O https://raw.githubusercontent.com/Cardio-Health/glaze-setup/main/docker/docker-compose.yml
 
 # Start the application
 docker-compose up -d
