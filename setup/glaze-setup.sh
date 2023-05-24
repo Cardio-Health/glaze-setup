20cb3b4aba9ac25c4ca9909a03e1b67499642a73
DoctorAti <atishayatishayjainjain@gmail.com>
2023-05-24 22:40:40 +0530

    changes

--- setup/glaze-setup.sh
+++ setup/glaze-setup.sh
@@ -12,6 +12,9 @@ curl -O https://raw.githubusercontent.com/Cardio-Health/glaze-setup/main/docker/
 export USER_KEY='<ENTER_YOUR_KEY HERE>'
 #Example: export USER_KEY='d20ebdc1c64fe1bc05407e04314847fc:696ec0af7761b223c5a9808256795b7d817410ce7b9c753d4e27poi8e2ebb0331fkijf387d6941802f450f4b2bdbe5ba498c08'
 
+export NEXT_PUBLIC_FRONTEND_URL=http://<your_host>:3000
+#Example: export NEXT_PUBLIC_FRONTEND_URL=http://localhost:3000
+
 # Start the application
 docker-compose up -d
 
