7d18a4ad6d36992ea39fecbfad933e2034928ff8
DoctorAti <atishayatishayjainjain@gmail.com>
2023-05-25 19:43:37 +0530

    changes

--- database/glaze-mysql-setup.sql
+++ database/glaze-mysql-setup.sql
@@ -98,8 +98,8 @@ CREATE TABLE `user_connectors` (
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-GRANT ALL PRIVILEGES ON main.* TO 'root'@'%' IDENTIFIED BY 'evolution';
+ALTER USER 'root'@'%' IDENTIFIED BY 'evolution';
+GRANT ALL PRIVILEGES ON main.* TO 'root'@'%';
 FLUSH PRIVILEGES;
-
 /*!40101 SET character_set_client = @saved_cs_client */;
 
