5b6031a4f24b9738caa05087cc1e474f7b7400af
DoctorAti <atishayatishayjainjain@gmail.com>
2023-07-26 15:58:31 +0530

    changes

--- setup/glaze-setup.sh
+++ setup/glaze-setup.sh
@@ -12,11 +12,13 @@ curl -O https://raw.githubusercontent.com/Cardio-Health/glaze-setup/main/docker/
 export USER_KEY='<ENTER_YOUR_KEY HERE>'
 #Example: export USER_KEY='d20ebdc1c64fe1bc05407e04314847fc:696ec0af7761b223c5a9808256795b7d817410ce7b9c753d4e27poi8e2ebb0331fkijf387d6941802f450f4b2bdbe5ba498c08'
 
-export NEXT_PUBLIC_FRONTEND_URL=http://<your_host>:3000
+export FRONTEND_URL=http://<your_host>
 #Example: export NEXT_PUBLIC_FRONTEND_URL=http://localhost:3000
+#Example: export FRONTEND_URL=http://ec2-43-204-98-46.ap-south-1.compute.amazonaws.com
 
 # Start the application
-docker-compose up 
+docker-compose up -d 
+#if sudo is required then sudo -E docker-compose up -d
 
 # please please wait for atleast 1 minute for the application to start , frontend may start early but won't work. so pleasee wait for 1 minute.
 # your application is live at: http://localhost:3000/dashboard/create
