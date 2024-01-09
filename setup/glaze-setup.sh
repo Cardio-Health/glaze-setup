c12f6d6872038ea567c2c3b6320e8ff71608ec3a
DoctorAti <atishayatishayjainjain@gmail.com>
2024-01-09 11:53:02 +0530

    changes

--- setup/glaze-setup.sh
+++ setup/glaze-setup.sh
@@ -23,3 +23,20 @@ docker-compose up -d
 #  wait for atleast 1 minute for the application to start 
 # your application is live at: http://localhost:3000/dashboard/create
 # your application is live at: http://ec2-43-204-98-46.ap-south-1.compute.amazonaws.com/dashboard/create
+
+
+
+#   - Always include http:// in your $FRONTEND_URL or any other host environment variable
+#   - Try to use nginx so that you can directly use your frontend on port 80, as they might forget to add 3000 to URLs every time.
+#   - Frontend service will always start on port 3000
+
+#   If you are deploying GlazeGPT in a dedicated machine and If you are using docker-compose, no need to export these environment variables: MANUAL_BACKEND_HOST, MANUAL_DATABASE_HOST, and MANUAL_DATABASE_PORT
+
+#   In case you want to deploy all 3 docker images in different networks, some information:
+#       - Backend service will always start on port 8000
+#       - MANUAL_BACKEND_HOST example: http://backend.glazegpt.abcd.com  or http://25.34.123.212:8000. if not on port 80, include port in this environment variable.
+#       - MANUAL_DATABASE_HOST example: http://database.glazegpt.abcd.com
+#       - MANUAL_DATABASE_PORT example: 5432
+#       - frontend health check GET API URL is: $FRONTEND_URL/api/glaze/healthcheck  
+#       - backend health check GET API URL is: $MANUAL_BACKEND_HOST/b/api/healthcheck 
+
